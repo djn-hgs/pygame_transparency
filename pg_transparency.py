d88cae9d097ab35d40cce0293e1bf59f5b6dca72
Daryl Noyce <daryl.noyce@highgateschool.org.uk>
2025-02-12 23:41:13 +0000

    Made some progress on blurring using surfaces with alpha channels...

--- pg_transparency.py
+++ pg_transparency.py
@@ -5,6 +5,7 @@ import pygame
 pygame.init()
 
 screen = pygame.display.set_mode((800, 600))
+screen.convert_alpha()
 
 start = pygame.time.get_ticks()
 
@@ -15,24 +16,44 @@ while looping:
         if event.type == pygame.QUIT:
             looping = False
 
-    # Clear screen
-
-    screen.fill((0, 0, 0))
-
     # Draw a rectangle
 
     new_time = pygame.time.get_ticks() - start
 
-    xpos = screen.get_width() / 2 + 200 * math.cos(new_time / 1000)
-    ypos = screen.get_height() / 2 + 200 * math.sin(new_time / 1000)
+    #surface = pygame.Surface(screen.get_size()).convert_alpha()
+
+    surface = screen.copy().convert_alpha()
+
+    surface.fill((0, 0, 0, 1))
+
+    #screen.fill((0, 0, 0, 1))
+
+    # pygame.draw.rect(surface,
+    #                  (0, 0, 0, 100),
+    #                  pygame.Rect(0, 0, surface.get_width(), surface.get_height())
+    #                  )
+
+    r = 200 + 20 * math.cos(new_time / 400)
+
+    xpos = screen.get_width() / 2 + r * math.cos(new_time / 1000)
+    ypos = screen.get_height() / 2 + r * math.sin(new_time / 1000)
 
     radius = 60 + 20 * math.sin(new_time / 200)
 
-    pygame.draw.circle(surface=screen,
-                       color=(255,255,255),
-                       center=(xpos,ypos),
-                       radius=radius
+    h = 180 + 180  * math.sin(new_time / 1000)
+    s = 50 + 50 * math.sin(new_time / 2000 + math.pi / 2)
+
+    mycolor = pygame.Color(0, 0, 0)
+    mycolor.hsva = (h, s, 100, 1)
+
+    pygame.draw.circle(surface=surface,
+                       color=mycolor,
+                       center=(xpos, ypos),
+                       radius=radius,
+                       width=0
                        )
+
+    screen.blit(surface, (0, 0))
 
     # Update display
 
